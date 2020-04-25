1f3a86f7ee8ebdce41b3e3eb7671e8f4b328cd90
Maria Alkhaja <38201407+mariaalkhaja@users.noreply.github.com>
2020-04-25 23:50:59 +0400

    linked list

--- common.h
+++ common.h
@@ -1,5 +1,8 @@
 #ifndef COMMON_H_INCLUDED
 #define COMMON_H_INCLUDED
+
+#include "packet.h"
+
 extern int verbose;
 
 
@@ -10,12 +13,20 @@ extern int verbose;
 #define DEBUG   0x100
 #define ALL     0x111
 
-#define VLOG(level, ... ) \
-    if(level & verbose) { \
-        fprintf(stderr, ##__VA_ARGS__ );\
-        fprintf(stderr, "\n");\
-    }\
+struct linked_list {
+  tcp_packet *pkt;
+  struct linked_list *next;
+};
+
+typedef struct linked_list *node;
+
+node create_node(tcp_packet *pkt);
+
+#define VLOG(level, ...)            \
+  if (level & verbose) {            \
+    fprintf(stderr, ##__VA_ARGS__); \
+    fprintf(stderr, "\n");          \
+  }
 
 void error(char *msg);
 #endif
-
