839ea46e139f87b876072a63daa266e2ef37ccc2
Maria Alkhaja <38201407+mariaalkhaja@users.noreply.github.com>
2020-04-25 23:50:05 +0400

    create node

--- common.c
+++ common.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include"common.h"
+
+#include "common.h"
+
 
 int verbose = ALL;
+
+
+/* empty node for buffer */
+node create_node(tcp_packet *pkt) {
+  node temp = (node)malloc(sizeof(struct linked_list));
+  temp->pkt = pkt;
+  temp->next = NULL;
+  return temp;
+}
+
 /*
  * error - wrapper for perror
  */
@@ -10,7 +22,4 @@ void error(char *msg) {
     perror(msg);
     exit(1);
 }
-
-
-
 
